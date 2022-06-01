983e4515f526aee9c00a7327dc6e5e8da2457a78
DESKTOP-QPJIEQC\admin <rkdruddud1@naver.com>
2022-06-02 00:50:03 +0900

    최초 세팅

--- app/src/main/java/com/example/capstonproject1/MainActivity.java
+++ app/src/main/java/com/example/capstonproject1/MainActivity.java
@@ -1,20 +1,39 @@
 package com.example.capstonproject1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mFirebaseAuth; // 파이어베이스 인증
+    private DatabaseReference mDatabaseRef; // 실시간 데이터베이스
+    private EditText mPw, mId;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("CapstonProject1");
+
+        mId = findViewById(R.id.editTextTextId);
+        mPw = findViewById(R.id.editTextTextPassword2);
 
         Button login = findViewById(R.id.loginButton);
         TextView idSearch = findViewById(R.id.idSearchtxt);
@@ -38,9 +57,25 @@ public class MainActivity extends AppCompatActivity {
                 }else {} */
                 /* 아이디와 패스워드 확인 코드 작성 */
 
-                Intent intent = new Intent(MainActivity.this, MainScreenActivity.class);
-                MainActivity.this.startActivity(intent);
-                MainActivity.this.finish();
+                String strId = mId.getText().toString();
+                String strPw = mPw.getText().toString();
+
+                mFirebaseAuth.signInWithEmailAndPassword(strId, strPw).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                   if(task.isSuccessful()){
+                       Intent intent = new Intent(MainActivity.this, MainScreenActivity.class);
+                       MainActivity.this.startActivity(intent);
+                       finish();
+                   }
+                   else
+                   {
+                       Toast.makeText(MainActivity.this, "로그인 실패", Toast.LENGTH_SHORT).show();
+                   }
+                    }
+                });
+
+
 
             }
         });
