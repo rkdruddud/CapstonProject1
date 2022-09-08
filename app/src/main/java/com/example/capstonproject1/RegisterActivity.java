637df378b4a41de9c23671491e5b0043c1310707
rkdruddud <rkdruddud1@naver.com>
2022-09-09 01:13:46 +0900

    최초 세팅

--- app/src/main/java/com/example/capstonproject1/RegisterActivity.java
+++ app/src/main/java/com/example/capstonproject1/RegisterActivity.java
@@ -16,6 +16,7 @@ import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,8 +29,8 @@ import org.json.JSONObject;
 public class RegisterActivity extends AppCompatActivity {
 
     private EditText mName, mPw, mPwTest, mHpNumber, mId;
-     Button mBtnRegister;
 
+// <uses-library android:name="org.json.JSONObject" android:required="false" />
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,52 +41,87 @@ public class RegisterActivity extends AppCompatActivity {
         mName = findViewById(R.id.editTextTextPersonName3);
         mHpNumber = findViewById(R.id.editTextPhone);
         mPwTest = findViewById(R.id.editTextTextPassword3);
-        mBtnRegister = findViewById(R.id.complitebtn);
+        Button mBtnRegister = findViewById(R.id.complitebtn);
+        Button doubleCheckBtn = findViewById(R.id.button2);
+        /*
+        doubleCheckBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userID = mId.getText().toString();
+                Response.Listener<String> responseListener2 = new Response.Listener<String>() {
 
-        String strId = mId.getText().toString();
-        String strPw = mPw.getText().toString();
-        String strPwTest = mPwTest.getText().toString();
-        String strName = mName.getText().toString();
-        String strHpNumber = mHpNumber.getText().toString();
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
 
-
+                            boolean success = jsonObject.getBoolean("success");
+                            if (success) {
+                                Toast.makeText(getApplicationContext(), "중복된 아이디가 존재합니다.", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(getApplicationContext(), "사용 가능한 아이디 입니다.", Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                RegisterRequest registerRequest2 = new RegisterRequest(userID, responseListener2);
+                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                queue.add(registerRequest2);
+            }
+        });*/
 
 
         mBtnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String userID = mId.getText().toString();
+                String userPassword = mPw.getText().toString();
+                String strPwTest = mPwTest.getText().toString();
+                String userName = mName.getText().toString();
+                String userPhonNumber = mHpNumber.getText().toString();
 
-Response.Listener<String> responseListener = new Response.Listener<String>(){
-    @Override
-    public void onResponse(String response){
-        try {
-            JSONObject jsonObject = new JSONObject(response);
-            boolean success = jsonObject.getBoolean("success");
-            if(success){
-                Toast.makeText(getApplicationContext(),"회원 등록에 성공하였습니다.",Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
-                RegisterActivity.this.startActivity(intent);
-            }else{
-                Toast.makeText(getApplicationContext(),"회원 등록에 실패하였습니다.",Toast.LENGTH_SHORT).show();
-            return;
-            }
-        }catch (JSONException e){
-            e.printStackTrace();
-        }
-    }
-};
-RegisterRequest registerRequest = new RegisterRequest(strId,strPw,strName,strHpNumber, responseListener);
+                if(strPwTest != userPassword){
+                    Toast.makeText(getApplicationContext(), "비밀번호가 다릅니다.", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+
+                            boolean success = jsonObject.getBoolean("success");
+                            if (success) {
+                                Toast.makeText(getApplicationContext(), "회원 등록에 성공하였습니다.", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                                RegisterActivity.this.startActivity(intent);
+                            } else {
+                                Toast.makeText(getApplicationContext(), "회원 등록에 실패하였습니다.", Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                RegisterRequest registerRequest = new RegisterRequest(userID, userPassword, userName, userPhonNumber, responseListener);
                 RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
                 queue.add(registerRequest);
+                }
             }
         });
+
     }
 
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-        switch (item.getItemId()){
-            case android.R.id.home:{
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home: {
                 finish();
                 return true;
             }
