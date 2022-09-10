25baff33bc1778e403add62d1490b9b43ca77b5b
rkdruddud <rkdruddud1@naver.com>
2022-09-10 21:35:52 +0900

    최초 세팅

--- app/src/main/java/com/example/capstonproject1/ChangePWActivity.java
+++ app/src/main/java/com/example/capstonproject1/ChangePWActivity.java
@@ -6,6 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class ChangePWActivity extends AppCompatActivity {
 
@@ -15,12 +25,52 @@ public class ChangePWActivity extends AppCompatActivity {
         setContentView(R.layout.activity_change_pwactivity);
 
         Button btn = findViewById(R.id.searchPW2);
+        EditText newPW = findViewById(R.id.editTextTextPassword);
+        EditText confirmPW = findViewById(R.id.editTextTextPassword4);
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent bintent = new Intent(ChangePWActivity.this, MainActivity.class);
-                ChangePWActivity.this.startActivity(bintent);
-                finish();
+
+                Intent inIntent = getIntent();
+                final String userID = inIntent.getStringExtra("userID");
+                final String userName = inIntent.getStringExtra("userName");
+                final String userPhonNumber = inIntent.getStringExtra("userPhonNumber");
+                String userPassword = newPW.getText().toString();
+                String confirmPassword = confirmPW.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+
+
+                            boolean success = jsonObject.getBoolean("success");
+                           if(!userPassword.equals(confirmPassword)){
+                               Toast.makeText(getApplicationContext(), "새 비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show();
+                           }
+                            else if (success) {
+                                Toast.makeText(getApplicationContext(), "비밀번호가 변경되었습니다.", Toast.LENGTH_SHORT).show();
+                                Intent bintent = new Intent(ChangePWActivity.this, MainActivity.class);
+                                ChangePWActivity.this.startActivity(bintent);
+                                finish();
+                            } else {
+
+                                Toast.makeText(getApplicationContext(), "비밀번호 변경에 오류가 발생했습니다.", Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                PasswordInfoRequest passwordInfoRequest= new PasswordInfoRequest(userID, userPassword, userName, userPhonNumber, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(ChangePWActivity.this);
+                queue.add(passwordInfoRequest);
+
+
             }
         });
     }
--- app/src/main/java/com/example/capstonproject1/PwSearchActivity.java
+++ app/src/main/java/com/example/capstonproject1/PwSearchActivity.java
@@ -8,6 +8,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class PwSearchActivity extends AppCompatActivity {
 
@@ -24,10 +32,45 @@ public class PwSearchActivity extends AppCompatActivity {
         pwsearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String userID = id.getText().toString();
+                String userName = name.getText().toString();
+                String userPhonNumber = phoneNumber.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+
+
+                            boolean success = jsonObject.getBoolean("success");
+
+                            if (success) {
+
+                                Intent searchintent = new Intent(PwSearchActivity.this, ChangePWActivity.class);
+                                searchintent.putExtra("userID", userID);
+                                searchintent.putExtra("userPhonNumber", userPhonNumber);
+                                searchintent.putExtra("userName", userName);
+                                PwSearchActivity.this.startActivity(searchintent);
+                                finish();
+                            } else {
+
+                                Toast.makeText(getApplicationContext(), "입력하신 정보가 올바르지 않습니다.", Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                ConfirmPWRequest confirmPWRequest= new ConfirmPWRequest(userID, userName, userPhonNumber, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(PwSearchActivity.this);
+                queue.add(confirmPWRequest);
+
+
                 /* 아이디 , 이름 , 핸드폰 번호 일치시 비밀번호 알려주는 기능 작성*/
-                Intent searchintent = new Intent(PwSearchActivity.this, ChangePWActivity.class);
-                PwSearchActivity.this.startActivity(searchintent);
-                finish();
+
             }
         });
     }
